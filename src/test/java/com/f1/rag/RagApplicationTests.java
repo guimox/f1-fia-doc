0a010d87e2f10feaecc15076ebd6547d0c01bd07
Guilherme Lopes <guilhermxlopes@gmail.com>
2025-07-29 20:35:44 +0200

    feat: initial project with setup

---
+++ src/test/java/com/f1/rag/RagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
